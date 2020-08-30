50edeec3e26fd5ede127c41d27cf5be374d8b006
VWO53 <64356181+VWO53@users.noreply.github.com>
2020-08-30 11:29:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,4 +8,4 @@ from config import Config
 
 
 def start(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hello [{update.message.from_user.first_name}](tg://user?id={update.message.from_user.id}) This is Telegram Attach Bot!\n\n```You Can Attach Any Telegram Media With Your Long text```\n\n**Do One By One Other Wise You Will Get Permanent Ban**\n\nchannel:- © @TG_BotZ\n\n/help For More Details", parse_mode="Markdown")
+  context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hello [{update.message.from_user.first_name}](tg://user?id={update.message.from_user.id}) This is Telegram Attach Bot!\n\n```You Can Attach Any Telegram Media With Your Long text```\n\n*Do One By One Other Wise You Will Get Permanent Ban*\n\nchannel:- © @TG_BotZ\n\n/help For More Details", parse_mode="Markdown")
